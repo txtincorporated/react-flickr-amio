d1c96c36d76dc3e97d94785f06cad562ccd951c8
Nathan P. Keene <npk@txtincorporated.com>
2017-08-30 21:13:36 -0700

    Add blank files and install Mapbox libraries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Home from './home';
+import Map from './map';
+import Nav from './nav';
+
 class App extends Component {
   render() {
     return (
---
+++ src/api.js
@@ -0,0 +1,4 @@
+const FLICKR_KEY = '49ab98f0131176cf7168bf57ca213ff9';
+const FLICKR_SECRET = 'a475bf30b9544209';
+
+const MBOX_TOKEN = 'pk.eyJ1IjoibmtlZW5lIiwiYSI6ImNqNnp4MnFmcjAxamkzM2wwZ2pmbm5mbmMifQ.ZYDaSF6qiHIy7hLUxLBU9w';
