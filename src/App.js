27ee726b07d49f0cee016ebb2689acc5a7fc5e89
Nathan P. Keene <npk@txtincorporated.com>
2017-08-31 16:45:41 -0700

    Start adding routes and page architecture

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Nav />
+        <Map />
+        <Sheet />
+        <Switch>
+          <Route path='/' exact component={ Home }/>
+          <Route path='/map' component={ Mapview }/>
+          <Route path='/recent' component={ Sheet }/>
+          <Route />
+        </Switch>
       </div>
     );
   }
--- src/api.js
+++ src/api.js
@@ -4,3 +4,13 @@ const FLICKR_SECRET = 'a475bf30b9544209';
 const MBOX_TOKEN = 'pk.eyJ1IjoibmtlZW5lIiwiYSI6ImNqNnp4MnFmcjAxamkzM2wwZ2pmbm5mbmMifQ.ZYDaSF6qiHIy7hLUxLBU9w';
 
 const FLICKR = 'https://api.flickr.com/services/rest';
+
+export const photoReq = () => {
+  let startDate = (Date.now() - 86400000)/1000 | 0;
+
+  return fetch(
+    `${FLICKR}/?method=flickr.photos.search&api_key=${FLICKR_KEY}&tags=landscape%2C+architecture&min_upload_date=${startDate}&sort=date-upload-asc&safe_search=1&content_type=1&media=photos&has_geo=1&extras=geo&per_page=250&format=json&nojsoncallback=1`
+  )
+    .then(res => res.json());
+
+};
