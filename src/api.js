b67347adc1e49fa803bbd7240f67d6ba3e09beec
Nathan P. Keene <npk@txtincorporated.com>
2017-08-30 21:27:43 -0700

    Add Flickr API URL

--- src/api.js
+++ src/api.js
@@ -2,3 +2,5 @@ const FLICKR_KEY = '49ab98f0131176cf7168bf57ca213ff9';
 const FLICKR_SECRET = 'a475bf30b9544209';
 
 const MBOX_TOKEN = 'pk.eyJ1IjoibmtlZW5lIiwiYSI6ImNqNnp4MnFmcjAxamkzM2wwZ2pmbm5mbmMifQ.ZYDaSF6qiHIy7hLUxLBU9w';
+
+const FLICKR = 'https://api.flickr.com/services/rest';
