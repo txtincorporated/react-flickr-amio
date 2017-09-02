28311d7fff3a72be4c94db1e0b7b36948f19e04b
Nathan P. Keene <npk@txtincorporated.com>
2017-09-02 01:48:39 -0700

    Set up photo display sheet grid el

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const FLICKR_SECRET = 'a475bf30b9544209';
 const MBOX_TOKEN = 'pk.eyJ1IjoibmtlZW5lIiwiYSI6ImNqNnp4MnFmcjAxamkzM2wwZ2pmbm5mbmMifQ.ZYDaSF6qiHIy7hLUxLBU9w';
 
 const FLICKR = 'https://api.flickr.com/services/rest';
+const FLICKR_CDN = `https://farm${ farm }.staticflickr.com/${ server }/${ id }_${ secret }.jpg`
 
 export const reqPhotos = () => {
   let startDate = (Date.now() - 86400000)/1000 | 0;
--- src/photodisplay.js
+++ src/photodisplay.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
+import { FLICKR_CDN } from './api';
+
 class Photodisplay extends Component {
   render() {
     const { farm, server, id, secret } = this.props;
+    const buildURL = (farm, server, id, secret) => {
+      const imgURL = FLICKR_CDN;
+      return imgURL;
+
+    };
 
     return (
       <div>
+        <img 
+          src={ buildURL() }
+          style={{
+            display:'inline-block', 
+            width: 400,
+            height: 400,
+
+          }}
+        />
         
       </div>
     );
