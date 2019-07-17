1e404f7801554c186e528d48817b97c166c0b2fd
Nathan P. Keene <npk@txtincorporated.com>
2019-07-16 23:15:37 -0700

    testThumbdisplay initial commit, setting up jest and starting thumb.test.js

---
+++ src/thumb.test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { shallow, mount, render } from 'Enzyme';
+import Thumbdisplay from './thumb';
+
+const wrapper = shallow(<Thumbdisplay />);
+
+import { photoClick } from './App';
+
+
+
