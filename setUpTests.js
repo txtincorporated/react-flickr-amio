1cf13c2a5b664d1fefc7e17f843bd503c9d60f50
Nathan P. Keene <npk@txtincorporated.com>
2019-07-30 22:16:58 -0700

    buggy babel jest enzyme shit

--- setUpTests.js
+++ setUpTests.js
@@ -1,4 +1,5 @@
-import { configure } from 'enzyme';
+import { React } from 'react'; 
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 configure({ adapter: new Adapter() });
--- src/thumb.test.js
+++ src/thumb.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { shallow, mount, render } from 'Enzyme';
 import Thumbdisplay from './thumb';
-import { photoClick } from './App';
+//import { photoClick } from './App';
 
-const wrapper = shallow( Thumbdisplay );
+//const wrapper = shallow( <Thumbdisplay /> );
 
-const photo = {};  //TODO:  Get photo object from query results in Postman
+//const photo = {};  //TODO:  Get photo object from query results in Postman
+
+it('renders without crashing', () => {
+  shallow(Thumbdisplay);
+
+})
 
 
