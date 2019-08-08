9f1289167f8e60a281f615b79ff6392d897ac5b3
Nathan P. Keene <npk@txtincorporated.com>
2019-08-07 21:37:58 -0700

    Still troubleshooting react-scripts babel-core incompatibility

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,22 @@
 module.exports = {
+  plugins: [
+    ['@babel/plugin-transform-modules-commonjs', { allowTopLevelThis: true }],
+    '@babel/plugin-transform-strict-mode',
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-proposal-class-properties',
+  ],
   presets: [
+    
+    '@babel/preset-react',
+    '@babel/preset-flow',
     [  
       '@babel/preset-env',
       {
+        shippedProposals: true,
         targets: {
           node: 'current',
         },
       },
     ],
-    '@babel/preset-react',
   ],
 };
